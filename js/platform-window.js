856adf187ea328ad9b8041d0bb677b1c2f07108a
adam-saland <adam.saland@openfin.co>
2021-03-23 10:56:56 -0400

    find clicked tab's x view

--- js/platform-window.js
+++ js/platform-window.js
@@ -2,12 +2,26 @@ export const CONTAINER_ID = 'layout-container';
 window.addEventListener("beforeunload", function (e) {
     var confirmationMessage = "\o/";
     console.log(e)
-    (e || window.event).returnValue = confirmationMessage;     // Gecko + IE
+        (e || window.event).returnValue = confirmationMessage;     // Gecko + IE
     return confirmationMessage;                                /* Safari, Chrome, and other
                                                                 * WebKit-derived browsers */
-  });
+});
 window.addEventListener('DOMContentLoaded', () => {
     // Before .50 AI version this may throw...
-    fin.Platform.Layout.init({containerId: CONTAINER_ID});
-    // document.querySelector('')
+    fin.Platform.Layout.init({ containerId: CONTAINER_ID });
+    const layoutDiv = document.querySelector(`#${CONTAINER_ID}`);
+    layoutDiv.addEventListener('click', async (e) => {
+        e.preventDefault()
+        if (fin.me.isWindow) {
+            const { target: { parentElement } } = e;
+            console.log(parentElement) 
+            const views = await fin.me.getCurrentViews();
+
+            console.dir({ VIEWS: views }, { depth: null, colors: true })
+            console.dir({ TARGET: e.target }, { depth: null, colors: true })
+            const clickedView = views.find((v) => (parentElement.id.includes && e.target.className === "lm_close_tab"))
+            console.dir({ CLICKED_VIEW: clickedView }, { depth: null, colors: true })
+        }
+
+    })
 });
